3c3a4dc7f1b3700b90cab262c27bc8b9404b7a39
Aaron <aaronisawesome025@gmail.com>
2023-01-21 15:48:03 -0500

    added links and will finish tomorrow

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-let upLine = document.getElementById("one");
-
-let things = document.querySelectorAll("up");
-
-let newThings = document.querySelectorAll("down");
-
-height = upLine.getBoundingClientRect().height;
-
-things.style.paddingTop = `${height / 2}px`;
-newThings.style.paddingBottom = `${height / 2}px`;
+let thirtythree = document.getElementById('33');
+let fifty = document.getElementById('50');
+let sixtytwo = document.getElementById('62');
+let sixtyfour = document.getElementById('64');
+let sixtyfive = document.getElementById('65');
+let sixtyseven = document.getElementById('67');
+let eighty = document.getElementById('80');
+let ninetysix = document.getElementById('96');
